f30051cc9fda995c5e7acd5247567ca38447ab7a
PhamMinhVuong1107 <100543300+PhamMinhVuong21020425@users.noreply.github.com>
2022-12-12 18:38:25 +0700

    Change Title

--- home.php
+++ home.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="">
     <meta name=" viewport " content=" width = device-width, initial-scale = 1 ">
-    <title>Document</title>
+    <title>Home</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
--- profile/clubprofile.php
+++ profile/clubprofile.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name=" viewport " content=" width = device-width, initial-scale = 1 ">
-    <title>Document</title>
+    <title>Club Profile</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
